49dc4401e8c89f7118413fa841feb6107b9cfef9
Anna Angelina <annaangeh169@gmail.com>
2025-12-21 17:55:48 -0300

    revisando models, painel administrativo e shell interativa do Django para os models

--- Learning_Logs/apps.py
+++ Learning_Logs/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class LearningLogsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Learning_Logs'
+    name = 'learning_logs'
