8bce0a40a5106bedecf2c1ded3428a95c16c10b5
Anna Angelina <annaangeh169@gmail.com>
2025-12-25 17:34:28 -0300

    Padronizando páginas com um arquivo base
    implementando código python no html e criando outra rota para verificar o base.html

--- Learning_Logs/views.py
+++ Learning_Logs/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'learning_logs/index.html')
+
+def anotacoes(request):
+    return render(request, 'learning_logs/atividades.html')
