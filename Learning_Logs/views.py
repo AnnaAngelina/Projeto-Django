6d8100b2df15c67fe7eb0fc46db70534576877ba
Anna Angelina <annaangeh169@gmail.com>
2026-01-08 18:04:49 -0300

    criando url e view de anotações e permitindo a visualização de mais detalhes sobre tópicos

--- Learning_Logs/views.py
+++ Learning_Logs/views.py
@@ -9,3 +9,9 @@ def atividades(request):
     topics = Topic.objects.order_by('data_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/atividades.html', context)
+
+def anotacoes(request, id_topic):
+    topic = Topic.objects.get(id = id_topic)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/anotacoes_topics.html', context)
--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/atividades', views.atividades, name='atividades')
+    path('atividades', views.atividades, name='atividades'),
+    path('anotacoes/<id_topic>/', views.anotacoes, name = 'anotacoes')
 ]
