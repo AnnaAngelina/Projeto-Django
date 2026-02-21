f8f49473546b20ce748c9b3415fc35c1b190c3a6
Anna Angelina <annaangeh169@gmail.com>
2026-02-21 17:21:11 -0300

    página de criar conta

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('anotacoes/<id_topic>/', views.anotacoes, name = 'anotacoes'),
     path('new_topic', views.new_topic, name = 'new_topic'),
     path('new_entry/<id_topic>/', views.new_entry, name= 'new_entry'),
-    path('edit_entry/<id_entry>', views.edit_entry, name='edit_entry')
+    path('edit_entry/<id_entry>', views.edit_entry, name='edit_entry'),
 ]
